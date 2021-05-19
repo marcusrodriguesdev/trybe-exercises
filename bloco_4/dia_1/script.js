47bd1ca241f42aa15767c54688e48e1dda9bda28
marcus <marcus.rodrigues10@hotmail.com>
2021-05-19 17:37:48 -0300

    atividade 1,2 e 3

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,21 @@
+// atividade 1
+let a = 5;
+let b = 2;
+let adicao = a + b;
+let subtracao = a -b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+// atividade 2
+let primeiro = 3;
+let segundo = 2;
+let comparado = primeiro > segundo;
+
+// atividade 3
+let valorOne = 4;
+let valorTwo = 2;
+let valorThree = 7;
+let comparada = valorTwo < valorThree > valorOne;
+
+console.log(comparada)
