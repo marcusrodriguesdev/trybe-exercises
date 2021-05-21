80212cc03f0cbafea0560955a4098552c87ae2b1
marcus <marcus.rodrigues10@hotmail.com>
2021-05-21 19:50:33 -0300

    primeiros exercicios

--- bloco_4/dia_3/exercise1.js
+++ bloco_4/dia_3/exercise1.js
@@ -1,11 +1,6 @@
 //  exercicio 1
-let n = 5;
-let simbolo = '*';
-let space =  '';
+let n = 4;
 
-for (let index = 0;  index < n; index += 1) {
-   space = space + simbolo;
-};
-for (let index = 0; index < n; index += 1) {
-   console.log(space)
-} 
+for(index = 0; index < n; index += 1){
+   console.log('****')
+}
