8144c4274ce50d2ca4e21b8dafb75b56786ddb7c
marcus <marcus.rodrigues10@hotmail.com>
2021-05-21 19:33:07 -0300

    exercicio 1

---
+++ bloco_4/dia_3/exercise1.js
@@ -0,0 +1,11 @@
+//  exercicio 1
+let n = 5;
+let simbolo = '*';
+let space =  '';
+
+for (let index = 0;  index < n; index += 1) {
+   space = space + simbolo;
+};
+for (let index = 0; index < n; index += 1) {
+   console.log(space)
+} 
