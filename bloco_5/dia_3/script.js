04ad7f5ba2a90b61b4360d9dd372d691ff26cc85
marcus <marcus.rodrigues10@hotmail.com>
2021-05-28 18:48:44 -0300

    exercise 1

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -15,3 +15,35 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
+// Exercico 3
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+let tagFather = document.getElementById('days');
+for(let index = 0; index < dezDaysList.length; index += 1) {
+  let result = dezDaysList[index];
+
+  let days = document.createElement('li');
+  tagFather.appendChild(days)
+  days.innerText = result; 
+  days.className = 'day'
+}
+
+holidays = document.getElementsByTagName('li');
+let day24 = document.getElementsByTagName('li')[34];
+let day25 = document.getElementsByTagName('li')[35];
+let day31 = document.getElementsByTagName('li')[41];
+day24.className = 'day holiday';
+day25.className = 'day holiday';
+day31.className = 'day holiday';
+
+let friday4 = document.getElementsByTagName('li')[14];
+let friday11 = document.getElementsByTagName('li')[21];
+let friday18 = document.getElementsByTagName('li')[28];
+let friday25 = document.getElementsByTagName('li')[35];
+friday4.className = 'day friday';
+friday11.className = 'day friday';
+friday18.className = 'day friday';
+friday25.className = 'day holiday friday';
+
+// 
+
