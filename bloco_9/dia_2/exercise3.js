a12bfd9f5c9b6d8d0c5195603ed77169f4ee68a2
marcus <marcus.rodrigues10@hotmail.com>
2021-07-07 19:19:37 -0300

    exercise 3

---
+++ bloco_9/dia_2/exercise3.js
@@ -0,0 +1,19 @@
+const promise = () => {
+  const myPromise =  new Promise((resolve, reject) => {
+    const array = [20 ** 2, 5 ** 2, 7 ** 2, 11 ** 2, 30 ** 2, 9 ** 2, 22 ** 2, 3 ** 2, 23 ** 2, 16 ** 2];
+    const someNumbers = array.reduce((acc, curr) => acc + curr, 0)
+    if (someNumbers < 8000) {
+      resolve(someNumbers)
+    }
+    reject()
+  });
+  myPromise
+  .then(someNumbers => {
+    const numeros = [2, 3, 5, 10];
+    const result = numeros.map((number) => someNumbers / number)
+    console.log(result);
+  })
+  .catch(error => console.log('Promise rejeitada'));
+}
+
+promise() 
