9adacd5617efd986967cc3f668b1a065589dcdbc
marcus <marcus.rodrigues10@hotmail.com>
2021-07-14 20:46:44 -0300

    exercise 3

---
+++ bloco_10/day_2/exercise3.test.js
@@ -0,0 +1,40 @@
+const { expect } = require("@jest/globals");
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      };
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+
+  
+  describe('getUserName - promise', () => {
+    describe('when the user id exists', () => {
+      it('returns the user name', async () => {
+        await getUserName(4)
+        expect('Mark').toEqual('Mark');
+      });
+    });
+  
+    describe('when the user id does not exists', () => {
+      it('returns an error', async () => {
+        try { 
+          await getUserName(2);
+         } catch (error) {
+           expect(error).toEqual({ error: 'User with 2 not found.' })
+         }
+      });
+    });
+  });
