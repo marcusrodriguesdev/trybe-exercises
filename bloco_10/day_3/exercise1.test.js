d53d0b9259eaf3ff82c5028fae914b893c6ca4fa
marcus <marcus.rodrigues10@hotmail.com>
2021-07-15 19:28:34 -0300

    exercise 1

---
+++ bloco_10/day_3/exercise1.test.js
@@ -0,0 +1,11 @@
+const service = require('./exercise1');
+
+describe('check response randomNumber function', () => {
+  test('teste', () => {
+    service.randomNumber = jest.fn().mockReturnValue(10);
+
+    expect(service.randomNumber()).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+});
