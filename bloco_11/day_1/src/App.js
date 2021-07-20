871aa7988fa70dedcff7251d681335e14f741548
marcus <marcus.rodrigues10@hotmail.com>
2021-07-20 18:50:11 -0300

    all exercises

--- bloco_11/day_1/src/App.js
+++ bloco_11/day_1/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+const items = ['Churrasco', 'Futebol', 'Truco'];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="ListItems">
+       {items.map(buscar => <ul>{Task(buscar)}</ul>)}
       </header>
     </div>
   );
 }
+
 
 export default App;
