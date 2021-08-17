42947d34f9510a2d4e07056a0023ec6805ebe3d8
marcus <marcus.rodrigues10@hotmail.com>
2021-08-17 15:57:07 -0300

    test of full aplication

--- bloco_15/day_2_testando-react-router/testes-react-router/src/App.test.js
+++ bloco_15/day_2_testando-react-router/testes-react-router/src/App.test.js
@@ -1,20 +1,21 @@
-// import React from 'react';
+import React from 'react';
 import App from './App';
-// import renderWithRouter from './renderWithRouter';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from "./renderWithRouter";
 
-// test('deve renderizar o component app', () => {
-//   const { getByText } = renderWithRouter(<App />);
-//   const home = getByText(/Você está na página Início/);
-//   expect(home).toBeInTheDocument();
-// });
+describe('teste de toda aplicacao', () => {
+  test('deve renderizar o component app', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Você está na página Início/);
+    expect(home).toBeInTheDocument();
+  });
 
-test('deve renderizar o component sobre', () => {
-  const { getByText, history } = renderWithRouter(<App/>);
-  fireEvent.click(getByText(/Sobre/i));
-  const pathname = history.location.pathname;
-  expect(pathname).toBe('/about');
-  const aboutAll = getByText(/Você está na página Sobre/);
-  expect(aboutAll).toBeInTheDocument();
+  test('deve renderizar o component sobre', () => {
+    const { getByText, history } = renderWithRouter(<App/>);
+    fireEvent.click(getByText(/Sobre/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+    const aboutAll = getByText(/Você está na página Sobre/);
+    expect(aboutAll).toBeInTheDocument();
+  });
 });
