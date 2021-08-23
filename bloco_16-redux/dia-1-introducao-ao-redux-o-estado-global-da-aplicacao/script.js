883cbb0ec4560567068427f35af8eac967ec66ad
marcus <marcus.rodrigues10@hotmail.com>
2021-08-23 18:33:46 -0300

    exercise 6

--- bloco_16-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
+++ bloco_16-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
@@ -3,8 +3,19 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
+
 const NEXT_COLOR = 'NEXT_COLOR';
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+const RANDOM_COLOR = 'RANDOM_COLOR';
 const reducer = (state = ESTADO_INICIAL, action) => {
   switch(action.type) {
     case NEXT_COLOR:
@@ -17,6 +28,12 @@ const reducer = (state = ESTADO_INICIAL, action) => {
         ...state,
         index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
       }
+    case RANDOM_COLOR:
+      return {
+        ...state,
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length,
+      }   
     default:
       return state;
   }
@@ -30,6 +47,10 @@ document.querySelector('#previous').addEventListener('click', () => {
 
 document.querySelector('#next').addEventListener('click', () => {
   store.dispatch({ type: NEXT_COLOR });
+});
+
+document.querySelector('#random').addEventListener('click', () => {
+  store.dispatch({ type: RANDOM_COLOR });
 });
 
 store.subscribe(() => {
