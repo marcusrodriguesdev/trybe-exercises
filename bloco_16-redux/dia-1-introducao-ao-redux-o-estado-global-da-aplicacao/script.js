ca0d418e9fc979ca651ce99c43ace6a9db615738
marcus <marcus.rodrigues10@hotmail.com>
2021-08-23 16:25:08 -0300

    execise 1 and 2

---
+++ bloco_16-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
@@ -0,0 +1,13 @@
+const ESTADO_INICIAL = {
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  index: 0,
+};
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer);
