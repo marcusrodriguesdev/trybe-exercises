cb108f0b9d7221020660bb778cd910709787d089
marcus <marcus.rodrigues10@hotmail.com>
2021-08-23 18:05:01 -0300

    exercise 3

--- bloco_16-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
+++ bloco_16-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
@@ -3,8 +3,20 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
+const NEXT_COLOR = 'NEXT_COLOR';
+const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
 const reducer = (state = ESTADO_INICIAL, action) => {
   switch(action.type) {
+    case NEXT_COLOR:
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1, 
+      }
+    case PREVIOUS_COLOR:
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      }
     default:
       return state;
   }
