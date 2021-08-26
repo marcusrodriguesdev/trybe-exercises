84bb18cfca4776e771dbf67cfb7d65925cb0e7e2
marcus <marcus.rodrigues10@hotmail.com>
2021-08-26 20:30:03 -0300

    finish page Home

--- bloco_16-redux/dia-3-usando-o-redux-no-react-pratica/login-page/src/pages/Home.jsx
+++ bloco_16-redux/dia-3-usando-o-redux-no-react-pratica/login-page/src/pages/Home.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Home.css'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        Home
+        <div className="welcome">
+          <h1
+            className="title-home"
+          >
+            Bem Vindo e Bem Vinda!
+          </h1>
+        </div>
+        <div>
+          <span className="cadastro">Já tem uma conta? </span>
+          <Link
+            to="login"
+            className="link"
+          >
+            Entre
+          </Link>
+        </div>
+        <div className="login-register">
+          <span className="cadastro">Increva-se! </span>
+          <Link
+            to="login"
+            className="link"
+          >
+            Registrar
+          </Link>
+        </div>
       </div>
     );
   }
