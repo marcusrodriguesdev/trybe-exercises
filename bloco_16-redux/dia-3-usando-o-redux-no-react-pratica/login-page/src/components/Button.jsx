08cd62be88b4ab28185f74333c10e69fee801093
marcus <marcus.rodrigues10@hotmail.com>
2021-08-26 21:44:04 -0300

    create component button

---
+++ bloco_16-redux/dia-3-usando-o-redux-no-react-pratica/login-page/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+  render() {
+    const { onClick, text, className } = this.props;
+    return (
+      <button
+        type="submit"
+        className={ className }
+        onClick={ onClick }
+      >
+        { text }
+      </button>
+    );
+  }
+}
