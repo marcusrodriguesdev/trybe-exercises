65bc48dd2ab08c475b17d3ea9a20a8f8a4f545dd
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-10-26 19:57:52 -0300

    finishing table creation

--- Back-end/bloco-21-normalizacao-e-modelagem-de-banco-de-dados/exercise.sql
+++ Back-end/bloco-21-normalizacao-e-modelagem-de-banco-de-dados/exercise.sql
@@ -18,3 +18,20 @@ CREATE TABLE Cuidador(
     FOREIGN KEY (gerente_id) REFERENCES Gerente (gerente_id)
 );
 
+CREATE TABLE Localizacao(
+	localizacao_id INT PRIMARY KEY auto_increment,
+    address VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE Animal(
+	nome VARCHAR(45) NOT NULL,
+	animal_id INT PRIMARY KEY auto_increment,
+    localizacao_id INT NOT NULL,
+    cuidador_id INT NOT NULL,
+    specie_id INT NOT NULL,
+    idade VARCHAR(45) NOT NULL,
+    sexo VARCHAR(45) NOT NULL,
+    FOREIGN KEY (localizacao_id) REFERENCES Localizacao (localizacao_id),
+    FOREIGN KEY (cuidador_id) REFERENCES Cuidador (cuidador_id),
+	FOREIGN KEY (specie_id) REFERENCES Specie (specie_id)
+);
