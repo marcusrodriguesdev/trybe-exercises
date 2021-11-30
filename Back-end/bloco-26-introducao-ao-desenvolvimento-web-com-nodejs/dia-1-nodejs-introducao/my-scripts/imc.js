93a87ead8fa7fb5e8f46e5d759f22ffb8dc3e43c
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-30 18:37:55 -0300

    exercise 3

--- Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/imc.js
+++ Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/imc.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
-
+const peso = readline.questionInt('Qual seu peso? (em kg)');
+const altura = readline.questionInt('Qual sua altura? (em cm)');
 
 const imc = (peso, altura) => {
   return peso / Math.pow(altura, 2).toFixed(2);
