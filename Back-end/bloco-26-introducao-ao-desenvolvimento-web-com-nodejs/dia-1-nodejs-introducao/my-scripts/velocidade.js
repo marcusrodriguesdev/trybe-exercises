b79bbe11432dd044362a9590d91abaa7482358b7
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-30 19:13:57 -0300

    exercise 7

--- Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/velocidade.js
+++ Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/velocidade.js
@@ -3,6 +3,6 @@ const readline = require('readline-sync');
 const distancia = readline.questionInt('Qual a distancia do veiculo?');
 const tempo = readline.questionInt('Quanto tempo este veiculo demorou?');
 
-const velocidade = (distancia / tempo);
+const velocidade = (distancia / tempo).toFixed(2);
 
 console.log(velocidade);
