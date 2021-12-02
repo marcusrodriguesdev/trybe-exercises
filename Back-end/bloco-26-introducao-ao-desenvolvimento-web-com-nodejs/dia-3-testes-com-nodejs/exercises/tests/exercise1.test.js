5fc962cbf4698640e2d48357928eecf018f3c9e1
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-02 20:22:06 -0300

    Exercise 1

---
+++ Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercises/tests/exercise1.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+const evaluatorNumber = require('../evaluatorNumber')
+
+describe('Essa função irá receber um número como parâmetro e retornar uma string como resposta', () => {
+  it('testa se o numero e positivo', () => {
+    const resposta = evaluatorNumber(4);
+    expect(resposta).to.be.equals('positivo');
+  });
+
+  it('testa se o numero e negativo', () => {
+    const resposta = evaluatorNumber(-4);
+    expect(resposta).to.be.equals('negativo');
+  })
+
+  it('testa se o numero e neutro', () => {
+    const resposta = evaluatorNumber(0);
+
+    expect(resposta).to.be.equal('neutro');
+  })
+});
