127a6ef71c2970f29465e2c2cebd5419e97fc356
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-03 19:04:09 -0300

    finish exercise 2

--- Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
+app.listen(3001, () => {
+  console.log('Aplicacao rodando na porta 3001');
+});
 
 
 // Exercise 1
 const createApiPing = (req, res) => {
   res.status(200).json({ message: 'pong' })
 }
-
 app.get('/ping', createApiPing);
 
-app.listen(3001, () => {
-  console.log('Aplicacao rodando na porta 3001');
-});
+
+// Exercise 2
+const createApiHello = (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}` })
+}
+app.post('/hello', createApiHello);
+
 
