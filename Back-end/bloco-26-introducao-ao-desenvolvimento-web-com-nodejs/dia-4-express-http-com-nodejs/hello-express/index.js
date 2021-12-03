8efd7903e07a173af464df968b4939c0f69414ed
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-03 16:17:17 -0300

    create my first api

---
+++ Back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+const orderName = drinks.sort((a, b) => {
+  if (a.name > b.name) return -1;
+});
+
+const createFirstApi = (req, res) => {
+  const { id, name, price } = req.body;
+  drinks.push({ id, name, price });
+  const drink = drinks.filter((d) => 
+    d.name.includes(name) && d.price < parseInt(maxPrice));
+  res.status(200).json(drink);
+}
+
+app.post('/drink/search', createFirstApi);
+
+app.listen(3001, () => {
+  console.log('Rodando na porta 3001');
+});
