29d1d0ee4e7f675d41922010d8934bd18335eada
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-05 15:32:55 -0300

    Request of type get

--- Back-end/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/hello-http-methods/index.js
+++ Back-end/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/hello-http-methods/index.js
@@ -1,1 +1,19 @@
 const axios = require('axios').default;
+
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+const headers = { Authorization: API_TOKEN };
+
+axios.get('https://postman-echo.com/get?param1=teste', { headers })
+  .then((response) => {
+    return response.data;
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((errorResponse) => {
+    if (errorResponse.status) {
+      return console.log(`Request failed with status ${errorResponse.status}`);
+    }
+    console.log(errorResponse);
+  });
